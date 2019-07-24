3a2780eb58b57f51a9f6ea401dfc8e7cb2213b40
Solodov <solodov-dev@gmail.com>
2019-07-24 15:54:49 +0300

    Rock Paper Scissor GUI version

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function getRandom(){
-	return Math.floor(Math.random() * 3);
-}
-
 function computerPlay(){
 	const move = ['rock', 'paper', 'scissors'];
-	return move[getRandom()];
+	return move[Math.floor(Math.random() * 3)];
 }
  
 function playRound(playerSelection, computerSelection){
@@ -27,17 +23,41 @@ function playRound(playerSelection, computerSelection){
 	}
 } 
 
-const player = document.querySelector('.player');
-const text = document.querySelector('p');
 
-player.querySelector('.rock').addEventListener('click', function(e){
-	text.innerHTML = playRound('rock', computerPlay());
-});
+let playerScore = 0;
+let computerScore = 0;
+const player = document.querySelectorAll('.player > *');
+const score = document.querySelector('#score');
+const btns = document.querySelectorAll('.nav > *');
 
-player.querySelector('.paper').addEventListener('click', function(e){
-	text.innerHTML = playRound('paper', computerPlay());
-});
+player.forEach(btn => btn.addEventListener('click', function(e){
+	
+	computerBtn = document.querySelector(`.computer > .${computerPlay()}`);
 
-player.querySelector('.scissors').addEventListener('click', function(e){
-	text.innerHTML = playRound('scissors', computerPlay());
-});
+	switch (playRound(e.target.className, computerBtn.className)) {
+		case 'winner':
+			playerScore++;
+		break;
+		case 'looser':
+			computerScore++;
+		break;
+	}
+
+	e.target.classList.add('selected');
+	computerBtn.classList.add('selected');
+
+	score.innerHTML = playerScore + " : " + computerScore;
+
+	if (playerScore === 5) {
+		score.innerHTML = 'You won!';
+		btns.forEach(element => element.style.display = 'none');
+	} else if (computerScore === 5) {
+		score.innerHTML = 'PC won!';
+		btns.forEach(element => element.style.display = 'none');
+	}
+	
+}));
+
+btns.forEach(btn => btn.addEventListener('transitionend', function(e){
+	e.target.classList.remove('selected');
+}));
