90a7f6ccf906badb0e7e96f0a4d575cd5330864f
Solodov <solodov-dev@gmail.com>
2019-07-27 18:54:49 +0300

    Removed cards. Added table

--- script.js
+++ script.js
@@ -1,56 +1,73 @@
 let myLibrary = [];
+let titles = ['Title', 'Author', 'Pages', 'Status', 'Remove'];
+let formIsOpen = false;
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
-  };
+  this.status = read;
 }
 
 function addBookToLibrary(Book) {
   myLibrary.push(Book);
 }
 
+function toggleRead() {
+  alert('fuck you!');
+}
+
 function render(bookList) {
-
-
-  bookList.forEach(book => {
-    let card = document.createElement("div");
-    card.className = "card";
-    let list = document.createElement("ul");
-    list.className = "list";
+  //Add table header
+  let row = document.createElement("tr");
+  row.className = "row";
+  for (title of titles) {
+    let th = document.createElement("th");
+    row.appendChild(th).innerHTML = title;
+  }
+  container.appendChild(row);
+  
+  //Add books
+  bookList.forEach(function(book, index) {
+    let row = document.createElement("tr");
+    row.className = "row";
+    row.setAttribute("data-index", index);
     for (let property in book) {
       if (book.hasOwnProperty(property)) {
-        if (property == 'info') break;
-        let listItem = document.createElement("li");
-        listItem.innerHTML = `${property.toUpperCase()} - ${book[property]}`;
-        list.appendChild(listItem);
+        let dataItem = document.createElement("td");
+        if (property == 'status') {
+          dataItem.innerHTML = (`<button onclick='toggleRead()'>${book[property]}</button>`);
+        } else {
+          dataItem.innerHTML = book[property];
+        }
+        
+        row.appendChild(dataItem);
       }
     }
-    card.appendChild(list);
-    container.appendChild(card);
+    let deleteItem = document.createElement("button");
+    deleteItem.innerHTML = '×';
+    deleteItem.onclick = toggleRead;
+    row.appendChild(deleteItem);
+
+    container.appendChild(row);
   });
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolien", 295, "not read yet");
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolien", 295, "not read");
 let fiftyShadesOfGray = new Book(
   "50 Shades of Gray",
   "E.L.James",
   116,
-  "not read yet"
+  "not read"
 );
-let theMagus = new Book("The Magus", "J.Fowles", 700, "reading");
+let theMagus = new Book("The Magus", "J.Fowles", 700, "read");
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(fiftyShadesOfGray);
 addBookToLibrary(theMagus);
 
-
 let container = document.querySelector("#container");
-let addBookBtn = document.querySelector("#addform");
+let addFormBtn = document.querySelector("#addform");
 let form = document.querySelector("#form");
 let inputFields = document.querySelectorAll("input");
 let inputTitle = document.querySelector("#title");
@@ -61,24 +78,39 @@ let inputRead = document.querySelector("#read");
 render(myLibrary);
 
 inputFields.forEach(input => {
-  input.addEventListener('focus', function(e){
-    e.target.value = '';
-  })
-})
+  input.addEventListener("focus", function(e) {
+    e.target.value = "";
+  });
+});
 
-function showForm(){
-  form.style.display = "block";
+function showForm() {
+  if (formIsOpen) {
+    form.style.display = "none";
+    addFormBtn.style.backgroundColor = "#5e4cd3";
+    addFormBtn.innerHTML = '+'
+    formIsOpen = false;
+  } else {
+    form.style.display = "block";
+    addFormBtn.style.backgroundColor = "#f14640";
+    addFormBtn.innerHTML = '×';
+    formIsOpen = true;
+  }
 }
 
-function hideForm(){
-  form.style.display = "none";
-}
+function newBook() {
+  let item = new Book(
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    inputRead.value
+  );
 
-function newBook(){
-    let item = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value);
-    addBookToLibrary(item);
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-    render(myLibrary);
+  addBookToLibrary(item);
+
+  while (container.firstChild) {
+    console.log(container.firstChild);
+    container.removeChild(container.firstChild);
+  }
+
+  render(myLibrary);
 }
