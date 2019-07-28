8dfd7f0285e5c2dfd3c30ba24ff7a2f7a3a58a75
Solodov <solodov-dev@gmail.com>
2019-07-28 20:23:26 +0300

    Event listener for generated elements

--- script.js
+++ script.js
@@ -12,32 +12,40 @@ const gameBoard = (function Gameboard() {
     }
   }
 
+  function checkWinner(row, col, sign) {
+    if (
+      (board[row][0] == sign &&
+        board[row][1] == sign &&
+        board[row][2] == sign) || //ckeck row
+      (board[0][col] == sign &&
+        board[1][col] == sign &&
+        board[2][col] == sign) || //check column
+      (board[0][0] == sign && board[1][1] == sign && board[2][2] == sign) || //check dioganals
+      (board[0][2] == sign && board[1][1] == sign && board[2][0] == sign)
+    ) {
+      alert("won!");
+    }
+
+    if (moves == 9) alert("tie!");
+  }
+
   function move(row, col) {
-      moves++;
+    moves++;
+
     if (isEmpty(row, col)) {
       board[row][col] = sign;
+
+      checkWinner(row, col, sign);
+
       if (sign == "o") {
         sign = "×";
-        return "o";
       } else {
         sign = "o";
-        return "×";
       }
+      return true;
     } else {
       return false;
     }
-  }
-
-  function checkWinner(row, col, sign) {
-      if ((board[row][0] == sign && board[row][1] == sign && board[row][2] == sign) || //ckeck row
-      (board[0][col] == sign && board[1][col] == sign && board[2][col] == sign) ||     //check column 
-      (board[0][0] == sign && board[1][1] == sign && board[2][2] == sign) ||           //check dioganals 
-      (board[0][2] == sign && board[1][1] == sign && board[2][0] == sign)) {
-        alert("won!");
-    }
-    
-    if(moves == 9) alert("tie!");
-    
   }
 
   return { board, move, checkWinner };
@@ -48,8 +56,9 @@ function Player(name) {
 }
 
 const displayController = (function Display() {
+  let container = document.querySelector("#container");
+
   function render(gameBoard) {
-    let container = document.querySelector("#container");
     let rowIndex = 0;
     for (let row of gameBoard.board) {
       let colIndex = 0;
@@ -66,29 +75,22 @@ const displayController = (function Display() {
     }
   }
 
-  function renderMove(row, col, move) {
-    let cell = document.querySelector(`[data-row='${row}'][data-col='${col}']`);
-    cell.innerHTML = move;
+  function clear(gameBoard) {
+    while (container.hasChildNodes()) {
+      container.lastChild.remove();
+    }
   }
-  return { render, renderMove };
-})();
 
-playerX = Player("John");
-player0 = Player("Mike");
+  return { render, clear };
+})();
 
 displayController.render(gameBoard);
 
-let playFields = document.querySelectorAll(".playfield");
-
-playFields.forEach(playfield => {
-  playfield.addEventListener("click", function(e) {
+document.querySelector("#container").addEventListener("click", function(e) {
+  if (event.target.tagName.toLowerCase() === "div") {
     let sign = gameBoard.move(e.target.dataset.row, e.target.dataset.col);
     if (!sign) return;
-    displayController.renderMove(
-      e.target.dataset.row,
-      e.target.dataset.col,
-      sign
-    );
-    gameBoard.checkWinner(e.target.dataset.row, e.target.dataset.col,sign);
-  });
+    displayController.clear(gameBoard);
+    displayController.render(gameBoard);
+  }
 });
