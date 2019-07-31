cbe058ec243a3cb25a896b03c874c4f0b0f3e01b
Solodov <solodov-dev@gmail.com>
2019-07-31 10:40:00 +0300

    Added random computer move

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+function Player(name, type, sign, lastMove) {
+  return { name, type, sign, lastMove };
+}
+
+let playerX = Player("Glenn", "human", "×");
+let playerO = Player("John", "human", "o");
+
 const gameBoard = (function Gameboard() {
   let board = [["", "", ""], ["", "", ""], ["", "", ""]];
-  let sign = "o";
-  let moves = 0;
+  let currentPlayer = playerX;
+  let moves = 1;
 
-  function reset() {
+  function clear() {
     board = [["", "", ""], ["", "", ""], ["", "", ""]];
   }
 
-  function isEmpty(row, col) {
+  function cellIsEmpty(row, col) {
     if (board[row][col] == "") {
       return true;
     } else {
@@ -15,48 +22,105 @@ const gameBoard = (function Gameboard() {
     }
   }
 
-  function checkWinner(row, col, sign) {
+  function changeCurrentPlayer() {
+    if (currentPlayer == playerX) {
+      currentPlayer = playerO;
+    } else {
+      currentPlayer = playerX;
+    }
+    console.log("Changed");
+  }
+
+  function computerRandomMove() {
+    //Check corners. If empty - return coordinates
+    for (let i = 0; i <= 3; i += 2) {
+      for (let j = 0; j <= 3; j += 2) {
+        if (cellIsEmpty(i, j)) {
+          return { row: i, col: j };
+        }
+      }
+    }
+
+    for (let i=0; i<=3; i++) {
+      for (let j=0; j<=3; j++) {
+        if(cellIsEmpty(i,j)) {
+          return { row: i, col: j };
+        }
+      }
+    }
+  }
+
+  function calculateComputerMove() {
+    // If the first move and the middle field is empty
+    if (moves == 1 && board[1][1] == "") {
+      return { row: 1, col: 1 };
+    }
+
+    // If the first move and he middle field is not empty
+    if (moves == 1 && board[1][1] == "×") {
+      return { row: 2, col: 2 };
+    }
+
+    // If the third move and the player played opposite corners
+    if (moves == 3 && board[1][1]) {
+      if (board[0][0] == "×" && board[2][2] == "×") {
+        return { row: 0, col: 1 };
+      }
+    }
+
+    return computerRandomMove();
+  }
+
+  function checkWinner(row, col) {
     if (
-      (board[row][0] == sign &&
-        board[row][1] == sign &&
-        board[row][2] == sign) || //ckeck row
-      (board[0][col] == sign &&
-        board[1][col] == sign &&
-        board[2][col] == sign) || //check column
-      (board[0][0] == sign && board[1][1] == sign && board[2][2] == sign) || //check dioganals
-      (board[0][2] == sign && board[1][1] == sign && board[2][0] == sign)
+      (board[row][0] == currentPlayer.sign &&
+        board[row][1] == currentPlayer.sign &&
+        board[row][2] == currentPlayer.sign) || //ckeck row
+      (board[0][col] == currentPlayer.sign &&
+        board[1][col] == currentPlayer.sign &&
+        board[2][col] == currentPlayer.sign) || //check column
+      (board[0][0] == currentPlayer.sign &&
+        board[1][1] == currentPlayer.sign &&
+        board[2][2] == currentPlayer.sign) || //check dioganals
+      (board[0][2] == currentPlayer.sign &&
+        board[1][1] == currentPlayer.sign &&
+        board[2][0] == currentPlayer.sign)
     ) {
       alert("won!");
     }
 
-    if (moves == 9) alert("tie!");
+    if (moves > 9) alert("tie!");
+  }
+  
+  function computerMove() {
+    let coordinates = calculateComputerMove();
+    console.log(coordinates);
+    board[coordinates.row][coordinates.col] = currentPlayer.sign;
+    checkWinner(coordinates.row, coordinates.col);
+    moves++;
+    changeCurrentPlayer();
   }
 
   function move(row, col) {
-    moves++;
-
-    if (isEmpty(row, col)) {
-      board[row][col] = sign;
-
-      checkWinner(row, col, sign);
-
-      if (sign == "o") {
-        sign = "×";
-      } else {
-        sign = "o";
-      }
+    console.log(moves);
+    if (cellIsEmpty(row, col)) {
+      board[row][col] = currentPlayer.sign;
+      checkWinner(row, col);
+      changeCurrentPlayer();
+      moves++;
       return true;
     } else {
       return false;
     }
   }
 
-  return {board, reset, move};
+  return { board, clear, move, computerMove };
 })();
 
 const displayController = (function Display() {
   let container = document.querySelector("#container");
 
+  //Render the game board
   function render(gameBoard) {
     let rowIndex = 0;
     for (let row of gameBoard.board) {
@@ -74,23 +138,27 @@ const displayController = (function Display() {
     }
   }
 
+  //Clear playing field of child elements before rendering
   function clear() {
     while (container.hasChildNodes()) {
       container.lastChild.remove();
     }
   }
 
-  function showTypeMenu(){
+  //Show menu with game type choices (Play with a computer or multiplayer)
+  function showTypeMenu() {
     document.querySelector("#new-game").style.display = "none";
     document.querySelector("#container").style.display = "none";
     document.querySelector("#game-type").style.display = "flex";
   }
-  
-  function showPlayersMenu(numOfPlayers){
+
+  //Show form to enter player names
+  function showPlayersMenu(numOfPlayers) {
     document.querySelector("#game-type").style.display = "none";
+
     let div = document.querySelector("#player-name-input");
     div.style.display = "flex";
-    
+
     let input1 = document.createElement("input");
     input1.setAttribute("placeholder", "Player X name");
     input1.id = "player-one-name";
@@ -100,7 +168,7 @@ const displayController = (function Display() {
     let input2 = document.createElement("input");
     input2.id = "player-two-name";
 
-    if(numOfPlayers == 1){
+    if (numOfPlayers == 1) {
       input2.value = "XOXO 3000";
       input2.disabled = true;
       playerO.type = "computer";
@@ -114,43 +182,38 @@ const displayController = (function Display() {
     let btn = document.createElement("button");
     btn.setAttribute("onclick", "displayController.startNewGame()");
     btn.innerHTML = "Start";
-    
+
     div.appendChild(btn);
   }
-  
-  function startNewGame(){
+
+  //Show menu to start a new game
+  function startNewGame() {
     let header = document.querySelector("h1");
 
-    if(document.querySelector("#player-one-name").value){
+    if (document.querySelector("#player-one-name").value) {
       playerX.name = document.querySelector("#player-one-name").value;
-    };
+    }
 
-    if(document.querySelector("#player-two-name").value){
+    if (document.querySelector("#player-two-name").value) {
       playerO.name = document.querySelector("#player-two-name").value;
-    };
+    }
 
     header.innerHTML = `${playerX.name} vs ${playerO.name}`;
     header.style.visibility = "visible";
 
     document.querySelector("#player-name-input").style.display = "none";
-    
+
     container.style.display = "grid";
     displayController.render(gameBoard);
   }
 
-  return {render, clear, showTypeMenu, showPlayersMenu, startNewGame};
+  return { render, clear, showTypeMenu, showPlayersMenu, startNewGame };
 })();
 
-function Player(name, type) {
-  return {name, type};
-}
-
-let playerO = Player("John", "human");
-let playerX = Player("Glenn","human");
-
 document.querySelector("#container").addEventListener("click", function(e) {
-  if (event.target.className === "playfield") { //change to classname
+  if (event.target.className === "playfield") {
     if (!gameBoard.move(e.target.dataset.row, e.target.dataset.col)) return;
+    if (playerO.type == "computer") gameBoard.computerMove();
     displayController.clear();
     displayController.render(gameBoard);
   }
