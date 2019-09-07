7129331f48b3ae640da5ce0e9b63855e51ac9cfa
Solodov <solodov-dev@gmail.com>
2019-09-07 10:51:31 +0300

    Tests passed

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,5 @@
-function capitalize(string) {
-    return 'Hello';
+function capitalize(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
 }
 
 module.exports = capitalize;
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,13 @@
 const capitalize = require('./capitalize')
 
-test('returns capitalized string', () => {
+test('capitalizes lowercase string', () => {
     expect(capitalize('hello')).toBe('Hello');
+})
+
+test('capitalizes uppercase string', () => {
+    expect(capitalize('HELLO')).toBe('Hello');
+})
+
+test('capitalizes sentence', () => {
+    expect(capitalize('this is the sentence')).toBe('This is the sentence');
 })
