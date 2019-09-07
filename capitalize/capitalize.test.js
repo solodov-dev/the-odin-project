527d73d70b333e9e254c4be7c4f716487e3ab398
Solodov <solodov-dev@gmail.com>
2019-09-07 11:00:17 +0300

    Folder structure

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -8,6 +8,10 @@ test('capitalizes uppercase string', () => {
     expect(capitalize('HELLO')).toBe('Hello');
 })
 
+test ('capitalizes uppercase/lowercase string', () => {
+    expect(capitalize('UnUsuAl')).toBe('Unusual');
+})
+
 test('capitalizes sentence', () => {
     expect(capitalize('this is the sentence')).toBe('This is the sentence');
 })
